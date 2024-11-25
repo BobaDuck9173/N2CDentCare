0fad1ed298b27bd82f6355185afbbcd7260349eb
BobaDuck9173 <bdc9173@gmail.com>
2024-11-25 15:18:58 +0700

    First commit

---
+++ src/main/java/com/example/N2CDentCare/controller/pageController.java
@@ -0,0 +1,47 @@
+package com.example.N2CDentCare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pageController {
+	@GetMapping("/index.html")
+	public String homePage() {
+		return "index";
+	}
+	
+	@GetMapping("/about.html")
+	public String aboutPage() {
+		return "about";
+	}
+	
+	@GetMapping("/contact.html")
+	public String contactPage() {
+		return "contact";
+	}
+	
+	@GetMapping("/price.html")
+	public String pricePage() {
+		return "price";
+	}
+	
+	@GetMapping("/service.html")
+	public String servicePage() {
+		return "service";
+	}
+	
+	@GetMapping("/team.html")
+	public String teamPage() {
+		return "team";
+	}
+	
+	@GetMapping("/appointment.html")
+	public String appointmentPage() {
+		return "appointment";
+	}
+	
+	@GetMapping("/testimonial.html")
+	public String testimonialPage() {
+		return "testimonial";
+	}
+}

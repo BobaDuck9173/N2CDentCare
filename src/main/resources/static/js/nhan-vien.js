124c7f43f9ed98fe450c89f6a3c93205c9ebf005
BobaDuck9173 <bdc9173@gmail.com>
2024-12-16 11:05:37 +0700

    thông tin bệnh nhân trên danh sách khi click vào sẽ hiển thị qua panel

--- src/main/resources/static/js/nhan-vien.js
+++ src/main/resources/static/js/nhan-vien.js
@@ -5,3 +5,19 @@ if(window.location.pathname == "/tim-benh-nhan"){
 function displayAddForm(id){
 	document.getElementById("add-" + id + "-form").style.display = "block";
 }
+
+function getBenhNhanInfo(id){
+	let position = id;
+	let ten = document.getElementById("ten-benh-nhan-" + position).innerHTML;
+	let gioiTinh = document.getElementById("gioi-tinh-benh-nhan-" + position).innerHTML;
+	let sdt = document.getElementById("sdt-benh-nhan-" + position).innerHTML;
+	document.getElementById("mini-panel-ten").innerHTML = ten;
+	document.getElementById("mini-panel-gioi-tinh").innerHTML = gioiTinh;
+	document.getElementById("mini-panel-sdt").innerHTML = sdt;
+	
+	console.log(position);
+	console.log(ten);
+	console.log(gioiTinh);
+	console.log(sdt);
+
+}
